17541d807e5187d607dfea5b9e79819585e18c2c
Marco <marco.schoch@gmx.ch>
2025-10-08 13:23:17 +0200

    initial commit

---
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,18 @@
+package example.cashcard;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1,2);
+	}
+
+	
+
+}
