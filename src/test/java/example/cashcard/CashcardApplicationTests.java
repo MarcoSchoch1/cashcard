ea4df6ae07cfb85ab84ba4f90e1d70223e4a6603
Marco <marco.schoch@gmx.ch>
2025-10-09 13:12:04 +0200

    #IMPROVEMENT add a H2 in memory DB and refactor the controller to use the DB to search for a CashCard with the given ID

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -2,8 +2,6 @@ package example.cashcard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.net.ssl.SSLEngineResult.Status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
